794085ebed09506dfc10f94db9db63171533da0c
setarehomadian <homadiansetare12@gmail.com>
2025-08-14 12:15:40 -0700

    commit theme btn

--- js/main.js
+++ js/main.js
@@ -2,18 +2,42 @@ const navBar = document.querySelector("#navBar");
 const ThemeBtn = document.querySelector("#ThemeBtn");
 const navSpan = document.querySelectorAll("#navBar > ul > li > span");
 const navLi = document.querySelectorAll("#navBar > ul > li");
+const mainSec = document.querySelector("#mainSec");
+const listener = document.querySelector("#listener");
+const hTitle = document.querySelector("#h-title");
+const searchBox = document.querySelector("#searchBox");
+const cardBox = document.querySelector(".card-box");
 
 // change theme
-click = 1;
+
+// change theme span////////////////////
 const sun = document.querySelector(".sun");
 const moon = document.querySelector(".moon");
+// change theme span////////////////////
+searchBox.classList.add("searchShadow", "ph-white");
+
+click = 1;
 ThemeBtn.addEventListener("click", () => {
   if (click % 2) {
     navBar.style.background = "var(--change-color-nav)";
     navSpan.forEach((s) => (s.style.color = "black"));
     sun.classList.add("hiddenSpan");
     moon.classList.remove("hiddenSpan");
+    navBar.classList.add("navShadow");
 
+    // mainSec
+    mainSec.style.background = "var(--change-color-section)";
+    listener.style.color = "black";
+    hTitle.style.color = "black";
+    // mainSec
+
+    // search box
+    searchBox.style.background = "var(--change-color-nav)";
+    searchBox.classList.remove("ph-white");
+    searchBox.classList.add("ph-black");
+    searchBox.style.color = ("black");
+    // search box
+   
     navLi.forEach((li) => {
       li.addEventListener("mouseenter", () =>
         li.querySelectorAll("span").forEach((s) => (s.style.color = "white"))
@@ -27,6 +51,20 @@ ThemeBtn.addEventListener("click", () => {
     navSpan.forEach((s) => (s.style.color = "white"));
     moon.classList.add("hiddenSpan");
     sun.classList.remove("hiddenSpan");
+
+    // search box
+    searchBox.style.background = "var(--main-color-nav)";
+    searchBox.classList.add("ph-white");
+    searchBox.classList.remove("ph-black");
+    searchBox.style.color = ("white");
+    // search box
+
+    // mainSec
+    mainSec.style.background = "var(--main-color-section)";
+    listener.style.color = "white";
+    hTitle.style.color = "white";
+
+    // mainSec
 
     navLi.forEach((li) => {
       li.addEventListener("mouseenter", () =>
